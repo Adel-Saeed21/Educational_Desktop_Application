d1c8861832e740a31c421799eaad2255500738f6
Adool <adelsaid284@gmail.com>
2025-07-29 22:10:13 +0300

    Remove unused assets and current quiz screen files

--- src/renderer/js/quiz_avialable.js
+++
@@ -1,7 +0,0 @@
-const startExamBtn = document.getElementById('StartExamButton');
-startExamBtn.addEventListener('click', () => {
-  window.api.startEXam().then(response => {
-    if (response.success) {
-      console.log('Exam started successfully');
-    } else {
-      console.error('Failed to start exam:', response.message); }})})
